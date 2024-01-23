31f18ba35dc43503ad7a1bc6b5b6a642be1d889e
unknown <pafrasson@gmail.com>
2024-01-22 22:21:18 -0300

    feat: os component v1

--- src/components/OSinput.js
+++ src/components/OSinput.js
@@ -2,7 +2,55 @@ import React from 'react'
 
 const OSinput = () => {
   return (
-    <div>OSinput</div>
+    <form className="bg-white p-6 border border-blue-500 rounded-md">
+      <div className="mb-4">
+        <label htmlFor="atendente" className="block text-black font-bold mb-2">Atendente:</label>
+        <select id="atendente" name="atendente" className="w-full p-2 border rounded-md">
+          
+          <option value="1">Atendente 1</option>
+          <option value="2">Atendente 2</option>
+          
+        </select>
+      </div>
+
+      <div className="mb-4">
+        <label htmlFor="cliente" className="block text-black font-bold mb-2">Cliente:</label>
+        <select id="cliente" name="cliente" className="w-full p-2 border rounded-md">
+          
+          <option value="1">Cliente 1</option>
+          <option value="2">Cliente 2</option>
+          
+        </select>
+      </div>
+
+      <div className="mb-4">
+        <label htmlFor="placa" className="block text-black font-bold mb-2">Placa do Veículo:</label>
+        <input type="text" id="placa" name="placa" className="w-full p-2 border rounded-md" placeholder="Digite a placa do veículo" />
+      </div>
+
+      <div className="mb-4">
+        <label htmlFor="produtos" className="block text-black font-bold mb-2">Produtos:</label>
+        <select id="produtos" name="produtos" multiple className="w-full p-2 border rounded-md">
+          
+          <option value="1">Produto 1</option>
+          <option value="2">Produto 2</option>
+        </select>
+      </div>
+
+      <div className="mb-4">
+        <label htmlFor="servicos" className="block text-black font-bold mb-2">Serviços:</label>
+        <select id="servicos" name="servicos" multiple className="w-full p-2 border rounded-md">
+          
+          <option value="1">Serviço 1</option>
+          <option value="2">Serviço 2</option>
+          
+        </select>
+      </div>
+
+      
+
+      <button type="submit" className="bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600">Enviar</button>
+    </form>
   )
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { motion } from "framer-motion";
 import OSinput from '@/components/OSinput';
+import AsterImg from '../../public/images/profile/astersofticon.png'
 
 export default function Home() {
   return (
@@ -13,7 +14,10 @@ export default function Home() {
       </Head>
       <main className='flex items-center text-dark w-full min-h-screen dark:text-light'>
         <Layout className='pt-0 md:pt-16 sm:pt-8'>
-          <div className="flex items-center justify-center w-full lg:flex-col">
+          <div className="flex items-center justify-center w-full flex-col">
+          <Image src={AsterImg} alt='PedroFrasson' className='w-20 h-auto md:inline-block md:w-20'
+                priority
+              />
             <OSinput></OSinput>
           </div>
         </Layout>
