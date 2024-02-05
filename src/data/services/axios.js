709f1d9c275fbb40da8a41b127f54ba37a9025a3
Wesley Werikis <wesleywerikis@outlook.com>
2024-02-05 16:41:40 -0300

    feat(axios): added to api baseURL

--- src/data/services/axios.js
+++ src/data/services/axios.js
@@ -1,27 +1,26 @@
-import { Axios } from "axios";
+import axios from "axios";
 
-const instance = Axios.create({
-    baseURL: '',
-    timeout: 5000,
+const instance = axios.create({
+  baseURL: 'http://localhost:8080',
+  timeout: 5000,
 });
 
-
 export const fetchData = async (endpoint) => {
-    try {
-        const response = await instance.get(endpoint);
-        return response.data;
-    } catch (error) {
-        console.error('Erro na requisição GET:', error.message);
-        throw error;
-    }
+  try {
+    const response = await instance.get(endpoint);
+    return response.data;
+  } catch (error) {
+    console.error('Erro na requisição GET:', error.message);
+    throw error;
+  }
 };
 
 export const sendData = async (endpoint, data) => {
-    try {
-        const response = await instance.post(endpoint, data);
-        return response.data;
-    } catch (error) {
-        console.error('Erro na requisição POST:', error.message);
-        throw error;
-    }
+  try {
+    const response = await instance.post(endpoint, data);
+    return response.data;
+  } catch (error) {
+    console.error('Erro na requisição POST:', error.message);
+    throw error;
+  }
 };
